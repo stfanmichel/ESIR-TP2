0d45c91ee611c289ec28f706c3296d5ffd86e7ff
Stephane Michel <st.fan.michel@gmail.com>
2019-01-30 19:18:02 +0100

    Finalisation STEP 2

--- index.js
+++ index.js
@@ -7,15 +7,25 @@ const fs = require('fs')
 const debug = require('debug')('tp2-server')
 
 const port = 8000
+const path = '.'
 
 debug('Booting')
 
 // Configure our HTTP server
 const server = http.createServer((req, res) => {
     debug(`Request received ${req.method} ${req.url}`)
-    const files = fs.readdirSync('.')
+    const files = fs.readdirSync(path)
+
+    const result = []
+    files.forEach(file => {
+        result.push({
+            name: file,
+            properties: fs.statSync(path + "/" + file)
+        })
+    })
+
     res.writeHead(200, {"Content-Type": "application/json"})
-    res.end(JSON.stringify(files))
+    res.end(JSON.stringify(result))
 })
 
 // Listen on port 8000, IP defaults to 127.0.0.1
