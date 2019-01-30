ede69786d1b7eeb6b90bb7b64b7d238375925fab
Stephane Michel <st.fan.michel@gmail.com>
2019-01-30 18:58:01 +0100

    Finalisation STEP 1 avec debug

--- index.js
+++ index.js
@@ -3,17 +3,23 @@
 */
 
 const http = require('http')
+const fs = require('fs')
+const debug = require('debug')('tp2-server')
 
-let nbRequest = 0
+const port = 8000
+
+debug('Booting')
 
 // Configure our HTTP server
 const server = http.createServer((req, res) => {
-    // TODO
-
+    debug(`Request received ${req.method} ${req.url}`)
+    const files = fs.readdirSync('.')
+    res.writeHead(200, {"Content-Type": "application/json"})
+    res.end(JSON.stringify(files))
 })
 
 // Listen on port 8000, IP defaults to 127.0.0.1
-server.listen(8000)
+server.listen(port)
 
-// Put a friendly message on the terminal
-console.log("Server running at http://127.0.0.1:8000/")
+// Server ready
+debug(`Server running at http://127.0.0.1:${port}/`)
